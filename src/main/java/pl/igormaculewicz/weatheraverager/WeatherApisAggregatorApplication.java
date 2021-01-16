57d41d0813822a9978f7a91668458ed3c4cd8a82
igor.maculewicz <igor.maculewicz@assecods.pl>
2021-01-16 17:30:55 +0100

    Renamed service

--- src/main/java/pl/igormaculewicz/weatheraverager/WeatherApisAggregatorApplication.java
+++ src/main/java/pl/igormaculewicz/weatheraverager/WeatherApisAggregatorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WeatherAveragerApplication {
+public class WeatherApisAggregatorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WeatherAveragerApplication.class, args);
+        SpringApplication.run(WeatherApisAggregatorApplication.class, args);
     }
 
 }
